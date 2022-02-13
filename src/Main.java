b110e6c4b25f2a5134a509313459a5b19ac9e450
LibroWu <731096310@qq.com>
2022-02-13 23:15:15 +0800

    give up remake liveness analysis

--- src/Main.java
+++ src/Main.java
@@ -23,14 +23,14 @@ public class Main {
     public static void main(String[] args) throws Exception {
         String name = "test\\test.mx";
         String llvmOutput = "test\\test.ll";
-        String asmOutput = "test\\test.s";
+        String asmOutput = "output.s";
         //String asmOutput = "output.s";
         //String name = "D:\\workspace\\libro_workspace\\archive\\Compiler-2021-testcases\\codegen\\e2.mx";
-        InputStream input = new FileInputStream(name);
+        //InputStream input = new FileInputStream(name);
         PrintStream out_llvm = new PrintStream(llvmOutput);
         PrintStream out_asm = new PrintStream(asmOutput);
         //OutputStream out = System.out;
-        //InputStream input = System.in;
+        InputStream input = System.in;
         try {
             RootNode ASTRoot;
             globalScope gScope = new globalScope(null);
