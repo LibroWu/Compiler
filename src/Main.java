bc749452c44243a8c1678984a71985d0739142d7
LibroWu <731096310@qq.com>
2022-04-06 00:38:11 +0800

    debuging

--- src/Main.java
+++ src/Main.java
@@ -21,16 +21,22 @@ import java.util.HashMap;
 
 public class  Main {
     public static void main(String[] args) throws Exception {
-        String name = "test\\test.mx";
+        //String name = "test\\test.mx";
         String llvmOutput = "test\\test.ll";
-        //String asmOutput = "test\\test.s";
-        String asmOutput = "output.s";
+        String asmOutput = "test\\test.s";
+        //String asmOutput = "output.s";
         //String name = "D:\\workspace\\libro_workspace\\archive\\Compiler-2021-testcases\\codegen\\e2.mx";
-        //inputStream input = new FileInputStream(name);
+        //InputStream input = new FileInputStream(name);
         PrintStream out_llvm = new PrintStream(llvmOutput);
         PrintStream out_asm = new PrintStream(asmOutput);
         //OutputStream out = System.out;
         InputStream input = System.in;
+        boolean semantic = false,codegen=false,optimize=false;
+        for (String arg : args) {
+            if (arg.equals("sema")) semantic = true;
+            else if (arg.equals("codegen")) codegen = true;
+            else if (arg.equals("opt")) optimize = true;
+        }
         try {
             RootNode ASTRoot;
             globalScope gScope = new globalScope(null);
@@ -48,15 +54,17 @@ public class  Main {
             gScope.setIdToDef(idToDef);
             gScope.setIdToFuncDef(idToFuncDef);
             new SymbolCollector(gScope).visit(ASTRoot);
-            SemanticChecker semanticChecker = new SemanticChecker(gScope);
-            semanticChecker.visit(ASTRoot);
-            if (semanticChecker.hasLambda) return;
+            if (semantic) {
+                SemanticChecker semanticChecker = new SemanticChecker(gScope);
+                semanticChecker.visit(ASTRoot);
+            }
+            if (!codegen) return;
             program pg = new program();
             new IRBuilder(pg, gScope,idToDef,idToFuncDef).visit(ASTRoot);
-            //new IRPrinter(System.out).visitProgram(pg);
+            new IRPrinter(System.out).visitProgram(pg);
             new Mem2Reg(pg).run();
-            //new IRPrinter(System.out).visitProgram(pg);
-            new IROptimizer(pg).run();
+            new IRPrinter(System.out).visitProgram(pg);
+            if (optimize) new IROptimizer(pg).run();
             new IRPrinter(out_llvm).visitProgram(pg);
             AsmPg asmPg = new AsmPg();
             new InstrSelector(asmPg).visitProgram(pg);
